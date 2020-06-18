926a893bfd3cf84435574034f01d35eb7eae499f
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-06-18 07:08:50 +0500

    2nd task done

--- Assignment # 21-25/app.js
+++ Assignment # 21-25/app.js
@@ -1,6 +1,16 @@
-var firstName = prompt("What is your first name?");
-var lastName = prompt("What is your last name?");
+// 1st Task
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
 
-var fullName = firstName + " " + lastName;
+// var fullName = firstName + " " + lastName;
 
-alert("Welcome " + fullName);
+// alert("Welcome " + fullName);
+
+
+// 2nd Task
+// var favPhone = prompt("What is your favorite Mobile Phone?");
+// document.write("<h3>My favorite Phone is : " + favPhone + "</h3><br>");
+// document.write("<h3>Length of the String : " + favPhone.length + "</h3>");
+
+
+// 3rd Task
