df922632d52c3135ceb3b0c122967751a7ac71cb
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-06-20 22:31:36 +0500

    Update app.js

--- Assignment # 26-30/app.js
+++ Assignment # 26-30/app.js
@@ -1,7 +1,22 @@
 // 1st Task
+// var number = prompt("Enter a Number (floating point) : ");
+// number = Number(number);
+// if(number > 0)
+// {
+//     document.write("Number : " + number + "<br>");
+//     document.write("Round off value : " + Math.round(number) + "<br>");
+//     document.write("Floor value : " + Math.floor(number) + "<br>");
+//     document.write("Ceil value : " + Math.ceil(number) + "<br>");
+// }
+// else {
+//     alert("Number should be non-negative.");
+// }
+
+
+// 2nd Task
 var number = prompt("Enter a Number (floating point) : ");
 number = Number(number);
-if(number > 0)
+if(number <= 0)
 {
     document.write("Number : " + number + "<br>");
     document.write("Round off value : " + Math.round(number) + "<br>");
@@ -9,9 +24,6 @@ if(number > 0)
     document.write("Ceil value : " + Math.ceil(number) + "<br>");
 }
 else {
-    alert("Number should be non-negative.");
+    alert("Number is non-negative.");
 }
-
-
-
 
