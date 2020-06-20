d563ff39538835d2d4b6ee24eaa226eab1ab8d0f
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-06-20 22:29:03 +0500

    update index.html and app.js

---
+++ Assignment # 26-30/app.js
@@ -0,0 +1,17 @@
+// 1st Task
+var number = prompt("Enter a Number (floating point) : ");
+number = Number(number);
+if(number > 0)
+{
+    document.write("Number : " + number + "<br>");
+    document.write("Round off value : " + Math.round(number) + "<br>");
+    document.write("Floor value : " + Math.floor(number) + "<br>");
+    document.write("Ceil value : " + Math.ceil(number) + "<br>");
+}
+else {
+    alert("Number should be non-negative.");
+}
+
+
+
+
